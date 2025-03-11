7d6427c6513e58bdc82b3857f7d20cb38c4fef7c
KreyaV <crashaw@gmail.com>
2025-03-11 19:31:54 +0100

    redirect za prismic

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = async () => {
   return {
     reactStrictMode: true,
+    redirects: async () => {
+      return [
+        {
+          source: "/admin",
+          destination: "https://opgbiskupovic.prismic.io/",
+          permanent: true,
+        },
+      ];
+    },
   };
 };
 
